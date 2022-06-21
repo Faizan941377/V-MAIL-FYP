49243af70e096239d22246e94f9ae3ec67ef4f18
Faizan941377 <Faizan941377@gmail.com>
2022-06-21 11:35:52 +0500

    room database insert & fetch done

--- app/src/main/java/com/nextsuntech/vmail/User/LoginActivity.java
+++ app/src/main/java/com/nextsuntech/vmail/User/LoginActivity.java
@@ -4,16 +4,21 @@ import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRON
 import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -29,6 +34,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.nextsuntech.vmail.Dashboard.DashboardActivity;
 import com.nextsuntech.vmail.R;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -38,6 +45,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     EditText emailET;
     EditText passwordET;
     TextView signUpTV;
+    TextView loginByVoice;
+    ImageView loginWithVoiceIV;
+    TextToSpeech textToSpeech;
     RelativeLayout loginBT;
     ProgressDialog progressDialog;
     private FirebaseAuth mAuth;
@@ -63,15 +73,134 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         passwordET = findViewById(R.id.et_login_password);
         signUpTV = findViewById(R.id.tv_login_signUp);
         loginBT = findViewById(R.id.bt_login);
+        loginWithVoiceIV = findViewById(R.id.voice_login);
+        loginByVoice = findViewById(R.id.tv_user_loginVoice);
+
 
         progressDialog = new ProgressDialog(this);
 
         loginBT.setOnClickListener(this);
         signUpTV.setOnClickListener(this);
+        loginWithVoiceIV.setOnClickListener(this);
 
         biometric();
 
     }
+
+    private void loginWithVoice() {
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), i -> {
+            if (i != TextToSpeech.ERROR) {
+                textToSpeech.setLanguage(Locale.UK);
+                String email = "what is your email";
+                textToSpeech.speak(email, TextToSpeech.QUEUE_ADD, null);
+
+                Handler handler = new Handler();
+                handler.postDelayed(this::openMic1, 2000);
+            }
+        });
+
+    }
+
+    private void openMic1() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
+
+        try {
+            startActivityForResult(intent, 1);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void openMic2() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
+
+        try {
+            startActivityForResult(intent, 2);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void openMic3() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
+
+        try {
+            startActivityForResult(intent, 3);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                emailET.setText(result.get(0).replaceAll("\\s+", "") + "@gmail.com");
+
+
+                String password = "what is your password";
+                textToSpeech.speak(password, TextToSpeech.QUEUE_ADD, null);
+                Handler handler = new Handler();
+                handler.postDelayed(this::openMic2, 2000);
+            }
+        }
+        if (requestCode == 2) {
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                passwordET.setText(result.get(0));
+
+                Handler handler = new Handler();
+                handler.postDelayed(this::confirmLoginDetails, 2000);
+            }
+        }
+        if (requestCode ==3){
+            if (resultCode == RESULT_OK && null != data){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                loginByVoice.setText(result.get(0));
+            }
+            if (loginByVoice.getText().toString().equals("yes")){
+                login();
+            }
+            if (loginByVoice.getText().toString().equals("no")){
+                String clearData = "we are clear your all provided data please try again";
+                textToSpeech.speak(clearData,TextToSpeech.QUEUE_ADD,null);
+                emailET.setText("");
+                passwordET.setText("");
+            }
+        }
+    }
+
+    private void confirmLoginDetails() {
+        String confirm = "please confirm your provided details";
+        textToSpeech.speak(confirm,TextToSpeech.QUEUE_ADD,null);
+        String email = "your email is";
+        textToSpeech.speak(email,TextToSpeech.QUEUE_ADD,null);
+        textToSpeech.speak(emailET.getText().toString(),TextToSpeech.QUEUE_ADD,null);
+        String password = "your password is";
+        textToSpeech.speak(password,TextToSpeech.QUEUE_ADD,null);
+        textToSpeech.speak(passwordET.getText().toString(),TextToSpeech.QUEUE_ADD,null);
+
+
+        String ask = "if you want to login please say yes or don't want to login say no";
+        textToSpeech.speak(ask,TextToSpeech.QUEUE_ADD,null);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this::openMic3, 18000);
+    }
+
+
 
     private void biometric() {
         BiometricManager biometricManager = BiometricManager.from(this);
@@ -119,7 +248,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 String email = sharedPreferences.getString("email", "data not found");
                 String password = sharedPreferences.getString("password", "data not found");
 
-                performAuth(email,password);
+                performAuth(email, password);
                 Toast.makeText(getApplicationContext(),
                         "Authentication succeeded!", Toast.LENGTH_SHORT).show();
             }
@@ -181,6 +310,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
                 finish();
                 break;
+
+            case R.id.voice_login:
+                loginWithVoice();
+                break;
         }
     }
 
@@ -211,6 +344,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                 Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
                                 finish();
                             } else {
+                                progressDialog.dismiss();
                                 Toast.makeText(LoginActivity.this, "Login Failed Please try Again", Toast.LENGTH_SHORT).show();
                             }
                         }
